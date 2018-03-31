66017a69ca7aae594a0959384848ef8e96dd7054
MarinPuizina <marinpuizina@hotmail.com>
2018-04-01 00:32:43 +0200

    Added Decorator Pattern

---
+++ src/main/java/decoratorPattern/starbuzz/condimentsDecorators/CondimentDecorator.java
@@ -0,0 +1,9 @@
+package main.java.decoratorPattern.starbuzz.condimentsDecorators;
+
+import main.java.decoratorPattern.starbuzz.beverages.Beverage;
+
+public abstract class CondimentDecorator extends Beverage {
+
+    public abstract String getDescription();
+
+}
---
+++ src/main/java/decoratorPattern/starbuzz/condimentsDecorators/Mocha.java
@@ -0,0 +1,23 @@
+package main.java.decoratorPattern.starbuzz.condimentsDecorators;
+
+import main.java.decoratorPattern.starbuzz.beverages.Beverage;
+
+public class Mocha extends CondimentDecorator {
+
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .20;
+    }
+
+}
